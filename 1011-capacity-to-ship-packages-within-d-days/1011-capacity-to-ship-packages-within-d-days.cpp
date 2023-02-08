92810158153966da9d0d20d342a415922c669cd2
shreyas1610 <78413286+shreyas1610@users.noreply.github.com>
2023-02-08 14:56:50 +0530

    Time: 86 ms (28.08%), Space: 45.6 MB (5.77%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int s = 0;
+    int sum = 0;
+        int n = weights.size();
+        int m = days;
+    
+    for(int i = 0; i<weights.size(); i++) {
+        sum += weights[i];
+    }
+    int e = sum;
+    int ans = -1;
+    int mid = s + (e-s)/2;
+    
+    while(s<=e)
+    {
+        if(isPossible(weights,n,m,mid)) {
+            //cout<<" Mid returned TRUE" << endl;
+            ans = mid;
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;   
+    }
+    
+    bool isPossible(vector<int> arr, int n, int m, int mid) {
+    int studentCount = 1;
+    int pageSum = 0;
+    //cout << "checking for mid "<< mid <<endl;
+    
+    for(int i = 0; i<n; i++) {
+        if(pageSum + arr[i] <= mid) {
+            pageSum += arr[i];
+        }
+        else
+        {
+            studentCount++;
+            if(studentCount > m || arr[i] > mid ) {
+            return false;
+        }
+            pageSum = arr[i];
+        }
+        if(studentCount > m) {
+            return false;
+        }
+        //cout << " for i " << i << " Student "<< studentCount << " pageSum " << pageSum << endl;
+    }
+    return true;
+}
+
+};
