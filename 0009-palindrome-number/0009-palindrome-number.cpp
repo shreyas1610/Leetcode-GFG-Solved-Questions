47557a86b192afc553202e135506f80cd864b0cb
Shreyas Singh <78413286+shreyas1610@users.noreply.github.com>
2023-02-19 23:12:25 +0530

    Time: 30 ms (14.83%), Space: 9.5 MB (6.98%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        bool pal = true;
+        vector<int> ans;
+        int b = 0;
+        if(x<0){
+            return false;
+        }
+      while(x>0){
+          b = x%10;
+          ans.push_back(b);
+          x = x/10;
+        }
+        int s =0;
+        int e = ans.size()-1;
+        while(s<=e){
+            if(ans[s]==ans[e]){
+                s++;
+                e--;
+            }else{
+                pal = false;
+                break;
+            }
+        }
+        
+        return pal;
+    }
+};
