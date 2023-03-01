ef31fbfe3c9a17ae95df77e88d442ace8b2079ad
Shreyas Singh <78413286+shreyas1610@users.noreply.github.com>
2023-03-01 11:57:34 +0530

    Time: 625 ms (19.93%), Space: 166.1 MB (12.99%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,14 +1,76 @@
 class Solution {
+    private:
+    void merge(vector < int > & arr, int s, int e) {
+
+    int mid = (s+e)/2;
+
+    int len1 = mid - s + 1;
+    int len2 = e - mid;
+
+    int *first = new int[len1];
+    int *second = new int[len2];
+
+    //copy values
+    int mainArrayIndex = s;
+    for(int i=0; i<len1; i++) {
+        first[i] = arr[mainArrayIndex++];
+    }
+
+    mainArrayIndex = mid+1;
+    for(int i=0; i<len2; i++) {
+        second[i] = arr[mainArrayIndex++];
+    }
+
+    //merge 2 sorted arrays     
+    int index1 = 0;
+    int index2 = 0;
+    mainArrayIndex = s;
+
+    while(index1 < len1 && index2 < len2) {
+        if(first[index1] < second[index2]) {
+            arr[mainArrayIndex++] = first[index1++];
+        }
+        else{
+            arr[mainArrayIndex++] = second[index2++];
+        }
+    }   
+
+    while(index1 < len1) {
+        arr[mainArrayIndex++] = first[index1++];
+    }
+
+    while(index2 < len2 ) {
+        arr[mainArrayIndex++] = second[index2++];
+    }
+
+    delete []first;
+    delete []second;
+
+}
+
+void Sort(vector < int > & arr, int s, int e) {
+
+    //base case
+    if(s >= e) {
+        return;
+    }
+    
+    int mid = (s+e)/2;
+
+    //left part sort karna h 
+    Sort(arr, s, mid);
+    
+    //right part sort karna h 
+    Sort(arr, mid+1, e);
+
+    //merge
+    merge(arr, s, e);
+
+}
 public:
     vector<int> sortArray(vector<int>& nums) {
-      
-        priority_queue<int, vector<int>, greater<int>> pq(nums.begin(), nums.end());
-
-            for(int i=0; i<nums.size(); i++)
-            {
-                nums[i] = pq.top();
-                pq.pop();
-            }
-            return nums;
+      int n = nums.size()-1;
+        Sort(nums, 0 ,n);
+        return nums;
     }
 };
