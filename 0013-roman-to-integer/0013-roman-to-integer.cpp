2c0383715e568bcd2e5896114c199c32e2f868fc
Shreyas Singh <78413286+shreyas1610@users.noreply.github.com>
2023-04-05 00:08:01 +0530

    Time: 9 ms (76.92%), Space: 6 MB (77.56%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        
+        int ans = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            switch (s.at(i)){
+                case 'I':
+                    if(i < s.length()-1 && s.at(i+1)=='V'){
+                        ans += 4;
+                        i++;
+                        break;
+                    }
+                    if(i < s.length()-1 && s.at(i+1)=='X'){
+                        ans += 9;
+                        i++;
+                        break;
+                    }
+                    ans += 1;
+                    break;
+                case 'V':
+                    ans += 5;
+                    break;
+                case 'X':
+                    if(i < s.length()-1 && s.at(i+1)=='L'){
+                        ans += 40;
+                        i++;
+                        break;
+                    }
+                    if(i < s.length()-1 && s.at(i+1) == 'C'){
+                        ans += 90;
+                        i++;
+                        break;
+                    }
+                    ans += 10;
+                    break;
+                case 'L':
+                    ans += 50;
+                    break;
+                case 'C':
+                    if(i < s.length()-1 && s.at(i+1)=='D'){
+                        ans += 400;
+                        i++;
+                        break;
+                    }
+                    if(i < s.length()-1 && s.at(i+1)=='M'){
+                        ans += 900;
+                        i++;
+                        break;
+                    }                    
+                    ans += 100;
+                    break;
+                case 'D':
+                    ans += 500;
+                    break;
+                case 'M':
+                    ans += 1000;
+                    break;
+                default:
+                    ans += 0;
+            }
+        }
+        
+        return ans;       
+    }
+};
