8f13a549020c6bd2628e066dadac5b24ba511ebe
Shreyas Singh <78413286+shreyas1610@users.noreply.github.com>
2023-05-14 16:20:55 +0530

    Time: 5 ms (73.01%), Space: 6.1 MB (8.83%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        vector<int> ans;
+        for(int i = 1;i*i<=num;i++){
+            if(num%i==0){
+                ans.push_back(i);
+                if((num/i)!=i){
+                    ans.push_back(num/i);
+                }
+            }
+        }
+        sort(ans.begin(), ans.end());
+        int ans1 = 0;
+        for(auto it:ans) ans1 = ans1+it;
+        
+        if((ans1-num)==num)
+            return true;
+        else
+            return false;
+        
+    }
+};
